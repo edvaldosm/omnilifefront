0a022abe6bd51b50610851fc3cd6d49fd5c2652f
Edvaldo Santiago <edvaldo.santiago@gmail.com>
2018-11-01 00:13:01 -0300

    Ajuste de formatacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from '@angular/core';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
-
 import { ClienteModule } from './cliente/cliente.module';
 import { AppRoutingModule } from './app.routing.module';
-// import { SuplementosModule } from './suplementos/rotas-modulos/suplementos.module';
 import { VMessageModule } from './shared/components/vmessage/vmessage.module';
-// import { PatologiaModule } from './patologia/rotas-modulos/patologia.module';
 import { HttpClientModule } from '@angular/common/http';
+// import { PatologiaModule } from './patologia/rotas-modulos/patologia.module';
+// import { SuplementosModule } from './suplementos/rotas-modulos/suplementos.module';
 
 
 
